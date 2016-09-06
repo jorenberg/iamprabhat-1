0677a762f7b176b0f7b7d883de9b9a3c2dad608d
Prabhat Kumar <super.dnaxy@gmail.com>
2016-09-06 13:40:02 +0530

    Initial commit.🇮🇳
    --/The Heart of Build System/-- of "AXIX®".
    Gruntfile.js, configuring the JavaScript Task Runner.
    Help: Help: https://github.com/gruntjs or http://gruntjs.com/ and http://gruntjs.com/configuring-tasks
    Usage: $ grunt build

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,1 +1,26 @@
 #!/usr/bin/env node
+
+
+/*!
+ * AXIX®
+ * GitHub     : https://github.com/AXIX
+ * With love for I'm Prabhat Kumar.
+ * ______________________________________________________________
+ * Grunt, http://gruntjs.com/.
+ * The JavaScript Task Runner.
+ * ______________________________________________________________
+ * Architecture and Code Handcrafted by Prabhat Kumar.
+ * Architectuur en Code handgemaakt door Prabhat Kumar.
+ * @author    : Prabhat Kumar [http://prabhatkumar.org/]
+ * @copyright : Sequømics Corporation [http://sequomics.com/]
+ * ______________________________________________________________
+ * @date      : 04-Sept-2016
+ * @license   : Apache, version 2.0
+ * @require   : Node.js®
+ * @require   : NPM
+ * @require   : Grunt
+ * ______________________________________________________________
+ *
+ * --/The Heart of Build System/-- of "AXIX®".
+ * ______________________________________________________________
+ */
