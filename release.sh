cacf37d700fb1e4efe87c3a44478c889b8dc4c8a
Prabhat Kumar <super.dnaxy@gmail.com>
2016-09-07 15:24:19 +0530

    Initial commit
    init.sh, shell script to --/release/-- whole "iamprabhat®" in one go.
    Usage: $ sh release.sh

---
+++ release.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# Tag and push a release.
+
+# =========================== I'm Prabhat® ===========================
+#               Technical Architect & Data Scientist
+# Handcrafted by Prabhat Kumar, http://prabhatkumar.org/.
+# Copyright © 2014 - 2016, Sequømics Corporation. All rights reserved.
+# ====================================================================
