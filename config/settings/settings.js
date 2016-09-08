f6da4aadfa54300095f056f13b6c9326b1cb14fa
Prabhat Kumar <super.dnaxy@gmail.com>
2016-09-08 13:21:59 +0530

    Initial commit
    Application settings to export "credentials/rules" in to Build System.

---
+++ config/settings/settings.js
@@ -0,0 +1,9 @@
+/*!
+ * Application Settings [+]
+ * ______________________________________________________________
+ * Architecture and Code Handcrafted by Prabhat Kumar.
+ * Architectuur en Code handgemaakt door Prabhat Kumar.
+ * @author    : Prabhat Kumar [http://prabhatkumar.org/]
+ * @copyright : Sequømics Corporation [http://sequomics.com/]
+ * ______________________________________________________________
+ */
