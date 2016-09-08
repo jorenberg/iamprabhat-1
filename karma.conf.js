ce1dfaa903b5bd17a4f7f916ea96997acc350140
Prabhat Kumar <super.dnaxy@gmail.com>
2016-09-09 01:05:59 +0530

    Initial commit
    --/The Heart of Test System/-- of "AXIX®".
    Help: https://github.com/karma-runner/karma

--- karma.conf.js
+++ karma.conf.js
@@ -1,1 +1,26 @@
 #!/usr/bin/env node
+
+
+/*!
+ * AXIX®
+ * GitHub     : https://github.com/AXIX
+ * With love for I'm Prabhat Kumar.
+ * ______________________________________________________________
+ * karma, https://github.com/karma-runner/karma
+ * The Spectacular Test Runner for JavaScript.
+ * ______________________________________________________________
+ * Architecture and Code Handcrafted by Prabhat Kumar.
+ * Architectuur en Code handgemaakt door Prabhat Kumar.
+ * @author    : Prabhat Kumar [http://prabhatkumar.org/]
+ * @copyright : Sequømics Corporation [http://sequomics.com/]
+ * ______________________________________________________________
+ * @date      : 09-Sept-2016
+ * @license   : Apache, version 2.0
+ * @require   : Node.js®
+ * @require   : NPM
+ * @require   : Grunt
+ * ______________________________________________________________
+ *
+ * --/The Heart of Karma Test Runner System/-- of "AXIX®".
+ * ______________________________________________________________
+ */
