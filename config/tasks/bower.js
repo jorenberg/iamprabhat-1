c5e86004c628ea19b705a5711669d49eb63b1982
Prabhat Kumar <super.dnaxy@gmail.com>
2016-09-08 13:46:19 +0530

    Initial commit
    Path: config/tasks/bower.js
    Usage: $ npm install grunt-wiredep --save-dev

---
+++ config/tasks/bower.js
@@ -0,0 +1,22 @@
+/*!
+ * iamprabhat®
+ * ____________________________________________________________________
+ * Grunt, The JavaScript Task Runner - http://gruntjs.com/.
+ * ~~~~~
+ * Bower          : ===================================================
+ * grunt-wiredep  : https://github.com/stephenplusplus/grunt-wiredep
+ * Usage          : $ npm install grunt-wiredep --save-dev
+ * @require       : .bowerrc and bower.json
+ * ____________________________________________________________________
+ * Architecture and Code Handcrafted by Prabhat Kumar.
+ * Architectuur en Code handgemaakt door Prabhat Kumar.
+ * @author    : Prabhat Kumar [http://prabhatkumar.org/]
+ * @copyright : Sequømics Corporation [http://sequomics.com/]
+ * ____________________________________________________________________
+ * @date      : 08-Sept-2016
+ * @license   : Apache, version 2.0
+ * @require   : Node.js®
+ * @require   : NPM
+ * @require   : Grunt
+ * ____________________________________________________________________
+ */
