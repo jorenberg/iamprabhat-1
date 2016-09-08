c9ed47921f11a6a815ca749ff0d1234696d55bcc
Prabhat Kumar <super.dnaxy@gmail.com>
2016-09-08 13:31:49 +0530

    Initial commit
    Path: config/tasks/autoprefixer.js
    Usage:
    1. $ npm install grunt-autoprefixer --save-dev
    2. $ npm install grunt-postcss --save-dev

---
+++ config/tasks/autoprefixer.js
@@ -0,0 +1,24 @@
+/*!
+ * iamprabhat®
+ * __________________________________________________________________
+ * Grunt, The JavaScript Task Runner - http://gruntjs.com/.
+ * ~~~~~~~~~~~~
+ * AutoPrefixer       : =============================================
+ * grunt-autoprefixer : https://github.com/nDmitry/grunt-autoprefixer
+ * Usage              : $ npm install grunt-autoprefixer --save-dev
+ * !
+ * grunt-postcss      : https://github.com/nDmitry/grunt-postcss
+ * Usage              : $ npm install grunt-postcss --save-dev
+ * __________________________________________________________________
+ * Architecture and Code Handcrafted by Prabhat Kumar.
+ * Architectuur en Code handgemaakt door Prabhat Kumar.
+ * @author    : Prabhat Kumar [http://prabhatkumar.org/]
+ * @copyright : Sequømics Corporation [http://sequomics.com/]
+ * __________________________________________________________________
+ * @date      : 08-Sept-2016
+ * @license   : Apache, version 2.0
+ * @require   : Node.js®
+ * @require   : NPM
+ * @require   : Grunt
+ * __________________________________________________________________
+ */
