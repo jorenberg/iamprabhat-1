dd66adc91a309c806c36e83c962773c1ff65ef39
Prabhat Kumar <super.dnaxy@gmail.com>
2016-09-08 11:48:11 +0530

    Initial commit
    Architecture + Design + Build with ♥ by Prabhat Kumar.
    CEO, Founder & Data Scientist - Sequømics Corporation.
    Copyright © 2014 - 2016 Sequømics Corporation, All Rights Reserved.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 #!/usr/bin/env node
+
+
+/*!
+ * iamprabhat®
+ * Designed and Build with ♥ for I'm Prabhat Kumar.
+ * ______________________________________________________________
+ * Architecture and Code Handcrafted by Prabhat Kumar.
+ * Architectuur en Code handgemaakt door Prabhat Kumar.
+ * @author    : Prabhat Kumar [http://prabhatkumar.org/]
+ * @copyright : Sequømics Corporation [http://sequomics.com/]
+ * ______________________________________________________________
+ * @date      : 04-Sept-2016
+ * @license   : Apache, version 2.0
+ * @require   : Node.js®
+ * @require   : NPM
+ * @require   : Grunt
+ * ______________________________________________________________
+ *
+ * --/The Heart of Core Build System/-- of "iamprabhat®".
+ * ______________________________________________________________
+ */
