fe51c63976540b797d0289dec80e13c61e81bf67
Prabhat Kumar <super.dnaxy@gmail.com>
2016-09-08 13:49:59 +0530

    Initial commit
    Path: config/tasks/clean.js
    Usage: $ npm install grunt-contrib-clean --save-dev

---
+++ config/tasks/clean.js
@@ -0,0 +1,20 @@
+/*!
+ * iamprabhat®
+ * ____________________________________________________________________
+ * Grunt, The JavaScript Task Runner - http://gruntjs.com/.
+ * ~~~~~~~~~~~~~~~~~~~
+ * grunt-contrib-clean : https://github.com/gruntjs/grunt-contrib-clean
+ * Usage               : $ npm install grunt-contrib-clean --save-dev
+ * ____________________________________________________________________
+ * Architecture and Code Handcrafted by Prabhat Kumar.
+ * Architectuur en Code handgemaakt door Prabhat Kumar.
+ * @author    : Prabhat Kumar [http://prabhatkumar.org/]
+ * @copyright : Sequømics Corporation [http://sequomics.com/]
+ * ____________________________________________________________________
+ * @date      : 08-Sept-2016
+ * @license   : Apache, version 2.0
+ * @require   : Node.js®
+ * @require   : NPM
+ * @require   : Grunt
+ * ____________________________________________________________________
+ */
