8ddb59100d810e4b2da5128652aa0f4770a9a97e
Prabhat Kumar <iamprabhat@users.noreply.github.com>
2016-09-11 01:22:02 +0530

    Initial commit
    Framework: AXIX® — https://github.com/AXIX

---
+++ source/core/script/main.js
@@ -0,0 +1,19 @@
+/*!
+ * iamprabhat®
+ * CEO, Founder & Data Scientist.
+ * _______________________________________________________________
+ * ITDB®, https://github.com/ITDB/ITDB
+ * The Non-AMD JavaScript - for defining "common" configurations.
+ * _______________________________________________________________
+ * Architecture and Code Handcrafted by Prabhat Kumar.
+ * Architectuur en Code handgemaakt door Prabhat Kumar.
+ * @author    : Prabhat Kumar [http://prabhatkumar.org/]
+ * @copyright : Sequømics Corporation [http://sequomics.com/]
+ * _______________________________________________________________
+ * @date      : 11-Sept-2016
+ * @license   : Apache, version 2.0
+ * @require   : Node.js®
+ * @require   : NPM
+ * @require   : Grunt
+ * _______________________________________________________________
+ */
